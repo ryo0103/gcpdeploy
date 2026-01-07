0bcc90b68e67545881fc8297a53a000f92598f75
ryo0103 <ryo241334@icloud.com>
2026-01-07 15:04:12 +0900

    minor fix for debug

--- app.py
+++ app.py
@@ -4,5 +4,4 @@ from google.cloud import storage as gcs
 st.title("GCS Mock")
 uploaded = st.file_uploader("ファイルを選択")
 client = gcs.Client()
-print(len(client.list_buckets()))
-print(client.list_buckets())
+[print(bucket) for bucket in client.list_buckets()]
