1f41f4ec1a803a1359a8ce74030d0688f4bcb144
ryo0103 <ryo241334@icloud.com>
2026-01-07 16:47:46 +0900

    fix app

--- GCSWrapper.py
+++ GCSWrapper.py
@@ -3,14 +3,19 @@ import os
 
 class GCSWrapper:
     def __init__(self):
+        bucket_name = os.environ["GCS_BUCKET_NAME"]
         self._client = gcs.Client()
-        self._bucket = self._client.get_bucket(os.environ["GCS_BUCKET_NAME"])
+        self._bucket = self._client.bucket(bucket_name)
 
     def get_file_names(self):
-        return [file for file in self._client.list_blobs(self._bucket)]
+        return [file.name for file in self._client.list_blobs(self._bucket)]
     
     def upload(self,file,file_name):
-        gcs_path = f"uploads/{file_name}"
+        gcs_path = f"{file_name}"
         blob = self._bucket.blob(gcs_path)
         blob.upload_from_file(file, rewind=True)
-        
+    
+    def download(self,file_name):
+        gcs_path = f"{file_name}"
+        blob = self._bucket.blob(gcs_path)
+        return blob.download_as_bytes()
--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import streamlit as st
-import os
 from GCSWrapper import GCSWrapper
 
-st.title("GCS Mock")
-uploaded = st.file_uploader("ファイルを選択")
 gcs = GCSWrapper()
+uploaded = st.file_uploader("アップロードするファイルを選択")
+
 if uploaded is not None:
     gcs.upload(uploaded,uploaded.name)
     for s in gcs.get_file_names():
         st.write(s)
+
+names = gcs.get_file_names()
+if not names:
+    st.write("ファイルがありません")
+else:
+    selected_file = st.selectbox("ダウンロードするファイルを選択",names)
+    if selected_file:
+        data = gcs.download(selected_file)
+        st.download_button(label="ローカルに保存",data=data,file_name=selected_file)
