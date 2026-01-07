a2b6deec519ebbfdc3d1e48e2f02c2ee4fe37bc5
ryo0103 <ryo241334@icloud.com>
2026-01-07 16:07:20 +0900

    test environ

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 from google.cloud import storage as gcs
+import os
 
 st.title("GCS Mock")
 uploaded = st.file_uploader("ファイルを選択")
 client = gcs.Client()
-st.write([bucket.name for bucket in list(client.list_buckets())])
+st.write(os.environ["GCS_BUCKET_NAME"])
+# st.write([bucket.name for bucket in list(client.list_buckets())])
