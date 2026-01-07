237054d83d4d1cbdd6711e6df02b279b67104050
ryo0103 <ryo241334@icloud.com>
2026-01-07 14:20:38 +0900

    dump

--- app.py
+++ app.py
@@ -4,6 +4,5 @@ from google.cloud import storage as gcs
 st.title("GCS テスト")
 project_id = "gcp-deploy-mock0106"
 uploaded = st.file_uploader("ファイルを選択")
-if uploaded is not None:
-    client = gcs.Client()
-    [print(bucket.name) for bucket in client.list_buckets()]
+client = gcs.Client()
+[print(bucket) for bucket in client.list_buckets()]
